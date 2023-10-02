23f1cfc45183003f5a57475edc14e055fa28fe4f
Gabriel <Gabriel.Fialho@domesticandgeneral.com>
2023-10-02 11:20:59 -0300

    fix: update account with signature verification

--- src/functions/api/updateAccount.ts
+++ src/functions/api/updateAccount.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { apiResponse } from "../../utils/api";
 import { getConnection } from "../../utils/database";
+import { ethers } from "ethers";
 
 export const handler = async (event: APIGatewayEvent) => {
   const { address } = event.pathParameters as unknown as { address: string };
@@ -9,6 +10,14 @@ export const handler = async (event: APIGatewayEvent) => {
   if (!event.body) return apiResponse(403, { message: "Body is required" });
 
   const { nickname, description } = JSON.parse(event.body);
+
+  const { Signature } = event.headers;
+
+  const signer = ethers.verifyMessage(event.body, Signature as any);
+
+  if (signer !== address)
+    return apiResponse(401, { message: "Not Authorized" });
+
   const prisma = getConnection();
 
   const user = await prisma.user.upsert({
